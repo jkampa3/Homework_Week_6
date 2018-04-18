b99db5befcc3f00a4b64f73f4e6f39a01e42fc04
unknown <Joseph.Kampa@gmail.com>
2018-04-18 05:43:29 -0500

    More JS Work

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,12 +1,12 @@
 //Variable
-var randomList = [];
+var randomList = ["britney spears", "beyonce" , "hristina aguilera" , "metallica" , "blink 182"];
 
 //API Call to GIPHY
 //function webAPIRetrieval() {
 
     var keyAPI = "bH52dsMR4iMXvtazjI8kQ4243fgBSwGX";
-    var movie = $(this).attr("data-name");
-    var queryAPIURL = "http://api.giphy.com/v1/gifs/search?api_key=" + keyAPI + "&q=" + movie + "&limit=10";
+    var searchText = $(this).attr("data-name");
+    var queryAPIURL = "http://api.giphy.com/v1/gifs/search?api_key=" + keyAPI + "&q=" + searchText + "&limit=10";
         
     $.ajax({
         url: queryAPIURL,
@@ -25,7 +25,7 @@ function renderButtons() {
     $("#buttonOutput").empty();
         for (var i = 0; i < randomList.length; i++) {
         var a = $("<button>");
-        a.addClass("movie-btn");
+        a.addClass("newGIPHYButton");
         a.attr("data-name", randomList[i]);
         a.text(randomList[i]);
         $("#buttonOutput").append(a);
@@ -33,9 +33,26 @@ function renderButtons() {
   };
 
 //Click Event to Add Button
+function clickSearch() {
+    
   $("#searchButton").on("click", function(event) {
         event.preventDefault();
-        var movie = $("#searchText").val().trim();
-        randomList.push(movie);
+        var searchInput = $("#searchText").val().trim();
+        randomList.push(searchInput);
         renderButtons();
   });
+};
+
+  //stop animation
+
+
+  //start animation
+
+
+
+
+  //Page Load Events
+
+  //$(document).on("click", ".newGIPHYButton", webAPIRetrieval);
+
+  renderButtons();
